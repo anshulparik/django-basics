bf0636994606b72e1a2c693c8f9e098c34a4fc27
anshulparik <anshulparik@gmail.com>
2024-06-10 11:13:38 +0530

    django admin, passing data to template and rendering it

--- myproject/posts/models.py
+++ myproject/posts/models.py
@@ -6,3 +6,6 @@ class Post(models.Model):
     body = models.TextField()
     slug = models.SlugField()
     date = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title

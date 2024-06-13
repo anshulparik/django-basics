8502ee3d64ae48ba1da19612921c5ec370973001
anshulparik <anshulparik@gmail.com>
2024-06-13 10:09:16 +0530

    file upload feature

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 urlpatterns = [
@@ -26,3 +28,5 @@ urlpatterns = [
     # including app urls
     path('posts/', include('posts.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

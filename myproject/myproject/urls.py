6beb6126e74c9c0da2aa10827b08b8b21aa10882
anshulparik <anshulparik@gmail.com>
2024-06-13 11:14:50 +0530

    added new app users

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -26,7 +26,8 @@ urlpatterns = [
     path('about/', views.about),
     
     # including app urls
-    path('posts/', include('posts.urls'))
+    path('posts/', include('posts.urls')),
+    path('users/', include('users.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
